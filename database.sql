5b216d339359c9e07e35fec6ff8b1776bc815d45
Aishwarya Joshi <aishwaryajoshi@dhcp96-136.calvin.edu>
2023-10-23 21:54:26 -0400

    changed user table to Users

--- database.sql
+++ database.sql
@@ -4,13 +4,13 @@
 
 -- Drop previous versions of the tables if they they exist, in reverse order of foreign keys.
 DROP TABLE IF EXISTS UserItem;
-DROP TABLE IF EXISTS "User";
+DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS Item;
 
 -- Create the schema.
 
 -- User is a reserved word, so we need to use double quotations around it "User"
-CREATE TABLE "User" (
+CREATE TABLE Users (
 	ID integer PRIMARY KEY, 
 	emailAddress varchar(50) NOT NULL,
 	name varchar(50),
@@ -29,7 +29,7 @@ CREATE TABLE Item (
 	);
 
 CREATE TABLE UserItem (
-	userID integer REFERENCES "User"(ID), 
+	userID integer REFERENCES Users(ID), 
 	itemID integer REFERENCES Item(ID),
     --could be change to another datatype
 	ownerfinder varchar(50)
@@ -38,13 +38,13 @@ CREATE TABLE UserItem (
 
 
 -- Allow users to select data from the tables.
-GRANT SELECT ON "User" TO PUBLIC;
+GRANT SELECT ON Users TO PUBLIC;
 GRANT SELECT ON Item TO PUBLIC;
 GRANT SELECT ON UserItem TO PUBLIC;
 
 -- Add sample records.
-INSERT INTO "User" VALUES (1, 'admin@calvin.edu', 'Admin', 'password', 'Admin');
-INSERT INTO "User" VALUES (2, 'aj37@calvin.edu', 'Aishwarya Joshi', 'password', 'Standard');
+INSERT INTO Users VALUES (1, 'admin@calvin.edu', 'Admin', 'password', 'Admin');
+INSERT INTO Users VALUES (2, 'aj37@calvin.edu', 'Aishwarya Joshi', 'password', 'Standard');
 
 INSERT INTO Item(ID, name, description, category, location, status) VALUES (1, 'Bottle', 'A blue 700ml bottle.', 'personal item', 'north hall', 'not claimed');
 INSERT INTO Item VALUES (2, 'Socks', 'A calvin sock.', 'personal item', 'north hall', 'not claimed');
