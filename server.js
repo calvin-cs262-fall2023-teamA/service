14eb1d567d1f3b0b2e6bf1463e327e2819442afa
Aishwarya Joshi <aishwaryajoshi@Aishwaryas-MacBook-Pro-2.local>
2023-10-25 22:17:37 -0400

    Azure integration

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 // Set up the database connection.
 
 const pgp = require('pg-promise')();
+// const db = pgp({
+//     host: 'peanut.db.elephantsql.com',
+//     port: 5432,
+//     database: 'nzykzast',
+//     user: 'nzykzast',
+//     password: 'PWMqmS1q0X0Q7ZfPZg-o3kODB6yDDu5x'
+// });
+
 const db = pgp({
-    host: 'peanut.db.elephantsql.com',
-    port: 5432,
-    database: 'nzykzast',
-    user: 'nzykzast',
-    password: 'PWMqmS1q0X0Q7ZfPZg-o3kODB6yDDu5x'
+    host: process.env.DB_SERVER,
+    port: process.env.DB_PORT,
+    database: process.env.DB_USER,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
 });
 
 // Configure the server and its routes.
 
 const express = require('express');
 const app = express();
-const port = 5432;
+const port = process.env.PORT || 3000;
 const router = express.Router();
 router.use(express.json());
 
@@ -38,7 +46,7 @@ function returnDataOr404(res, data) {
 }
 
 function readHelloMessage(req, res) {
-    res.send('Hello, People service!');
+    res.send('Welcome to Calvin Finds!');
 }
 
  function readUsers(req, res, next) {
