1537913736936981d723def321e795680472b95f
Aishwarya Joshi <aishwaryajoshi@dhcp103-124.calvin.edu>
2023-10-30 21:20:51 -0400

    remove id when creating user

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ function updateUser(req, res, next) {
 }
 
 function createUser(req, res, next) {
-    db.one('INSERT INTO Users(id, name, emailAddress, password, type) VALUES (${name}, ${email}, ${password}, ${type}) RETURNING id', req.body)
+    db.one('INSERT INTO Users(name, emailAddress, password, type) VALUES (${name}, ${email}, ${password}, ${type}) RETURNING id', req.body)
         .then(data => {
             res.send(data);
         })
